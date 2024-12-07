99ad1c4b8ec2cbbf99c9f8d0457ba73d6f8e4032
vjy-07 <gvijaycrl@gmail.com>
2024-12-08 00:09:00 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/progressTracker/", // your base URL
+  base: "/ProgressTracker/", // your base URL
   server: {
     open: true,  // This will automatically open the browser
   }
